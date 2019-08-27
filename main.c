d869a17c66f66b5261350f73374a4684749539c9
Arata Yano <aratayano00@gmail.com>
2019-08-27 17:42:38 +0200

    added some prints to check, see you tomorrow :lemon:

--- main.c
+++ main.c
@@ -2,11 +2,22 @@
 
 int main()
 {
-	t_anthill *anthill;
-	//printf("Room count : %d\n", get_nb_rooms());
+	t_anthill	*anthill;
+	t_room		*cursor;
 	anthill = build_anthill();
-	// printf("Anthill->linear = %p\n", anthill->linear);
-
-	// db_print_linear(anthill);
+	cursor = anthill->linear;
+	printf("ANTS : %d\n", anthill->nb_ants);
+	printf("ROOM COUNT : %d\n\n", anthill->room_count);
+	while (cursor->next != NULL)
+	{
+		printf("NAME : %s\n", cursor->name);
+		printf("X POS : %d\n", cursor->x);
+		printf("Y POS : %d\n", cursor->y);
+		printf("IS_END : %d\n", cursor->is_end);
+		printf("IS_START : %d\n", cursor->is_start);
+		printf("LINK COUNT : %d\n", cursor->link_count);
+		printf("\n\n");
+		cursor = cursor->next;
+	}
 	return 1;
 }
